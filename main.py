a6a18ece1a99788c6f69784a40fb995ad6b60a50
Sowmya Aji <sowmya.aji@gmail.com>
2022-06-27 23:36:47 -0400

    feature: added intro card and score

--- main.py
+++ main.py
@@ -18,13 +18,50 @@ window.setup(width=WIDTH, height=HEIGHT)
 # tracer(0) disables automatic screen updates (we'll be updating the screen ourselves)
 window.tracer(0)
 
-# Setup the turtle
+
+def set_up_turtles(turtle_name: str, message: str):
+    """Function to set up multiple turtle as individual cards for game and team intro
+    @params: name of the turtle to be created and the message it puts on the card
+    @returns: None"""
+    # set up intro turtle:
+    turtle_name = turtle.Turtle()
+    turtle_name.color('red')
+    style = ('Courier', 12, 'italic')
+    turtle_name.write(message,
+                font=style, align='center')
+    turtle_name.speed(5) 
+    # Let us see how many rounds you go!',
+    turtle_name.penup()
+    turtle_name.hideturtle()
+    time.sleep(2)
+    turtle_name.clear()
+
+def call_intro():
+    # set up and run the intro turtles dict:
+    turtle_msgs = { "intro_t1": "Come play Simon Says with us!",
+                "intro_t2": "Click on each tile in the same sequence",
+                "intro_t3": "Lets see how many rounds you go!",
+                "intro_t4": "Team:",
+                "intro_t5": "Matthew Barneto",
+                "intro_t6" : "Kristena Bridges",
+                "intro_t7" : "Jonathan Alvarado",
+                "intro_t8" : "Sowmya Aji",
+                "intro_t9" : "Have fun!"
+            }
+    # loop through list and dict to generate series of intro cards
+    for tur in turtle_msgs.keys():
+        set_up_turtles(tur, turtle_msgs[tur])
+        
+
+call_intro()
+# Setup the tiles turtle
 t = turtle.Turtle()
 # Make it draw instantly
 t.speed(0)
 # Hide the turtle
 t.penup()
 t.hideturtle()
+
 
 # Setup tiles
 tile_top_left = Tile(-SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE, "Top Left")
@@ -49,6 +86,15 @@ state = "Playing Sequence"
 sequence = ListQueue([0, 1, 3])
 player_seq = ListQueue(sequence.items)
 clicked_tile = None
+
+
+# set up score turtle
+score_t = turtle.Turtle()
+score_t.penup()
+score_t.setposition(0,0)
+score_t.hideturtle()
+score_t.color("red")
+score_t.width(10)
 score = 0
 
 
@@ -86,6 +132,7 @@ def play_sequence():
 # Try block to catch closing the window unexpectedly
 try:
     while True:
+        
         # Draw each tile
         for j in tiles:
             j.draw(t)
@@ -99,6 +146,7 @@ try:
             play_sequence()
             # Copy the sequence to a player_seq listqueue
             player_seq = ListQueue(sequence.items)
+            
             # Add a new number to the sequence
             sequence.add(random.randrange(0, 4))
             # Change the state to player's turn
@@ -128,5 +176,13 @@ try:
                 state = "Playing Sequence"
                 # Add 1 to score
                 score += 1
+                # show the score constantly on the top of the screen
+                window.title(f"Simon says: Player current score: { score }")
+                t.hideturtle()
+                # show the score in the middle of the screen for each round
+                score_t.write(f"Score: { score }", font=('Arial', 15, 'bold'), align="center")
+                time.sleep(1)
+                score_t.clear()
+                
 except turtle.Terminator:
     print("Player ended the game.")
